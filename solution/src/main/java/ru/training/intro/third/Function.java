0f3506a35597833fb44708116179d07a624fda61
Narek <narek.27.02.93@mail.ru>
2016-12-12 01:40:45 +0300

    First commit

---
+++ solution/src/main/java/ru/training/intro/third/Function.java
@@ -0,0 +1,46 @@
+package ru.training.intro.third;
+
+import static java.lang.Math.tan;
+
+/**
+ * Created by Narek on 12.12.2016.
+ */
+class Function {
+
+    double a;   // a - начало отрезка на котором вычисляется функция
+    double b;   // b - конец отрезка на котором вычисляется функция
+    double h;   // h - шаг дискретизации
+
+    protected Function(double a, double b, double h){
+        this.a=a;
+        this.b=b;
+        this.h=h;
+    }
+
+    /*Метод вычисляет значение функции
+    * на отрезке [a, b] с шагом h и выводит
+    * результаты в таблицу*/
+
+    protected strictfp void values(){
+        /*Выделяем количество итераций,
+        * берем всю длину отрезка и смотрим сколько
+        * шагов целочисленно входит в данный отрезок.
+        * (т.к. цикл for с типами double работает некорректно)
+        * */
+        int amount=(int)((b-a)/h);
+        double f=0; // значение функции
+        printHead(); // выводит заголовок таблицы x и f(x)
+        for(int i=amount; i>=0; i--, a+=h){
+            f=tan(2*a)-3;
+            printValues(a,f); // вывод аргумента и значения функции в таблицу
+        }
+    }
+
+    private static void printHead(){
+        System.out.printf("%5s  %12s%n","x", "f(x)");
+    }
+
+    private static void printValues(double x, double f){
+        System.out.printf("%6.2f %13.4f%n", x, f);
+    }
+}
---
+++ solution/src/main/java/ru/training/intro/third/Main.java
@@ -0,0 +1,13 @@
+package ru.training.intro.third;
+
+import static java.lang.Math.PI;
+
+/**
+ * Created by Narek on 12.12.2016.
+ */
+class Main {
+    public static void main(String[] args){
+        Function function=new Function(0, PI, 0.01);
+        function.values();
+    }
+}
